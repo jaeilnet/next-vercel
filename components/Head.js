bc6c013649563265ae052e7aa62244aca54be803
jaeilnet <sgsgself@naver.com>
2022-04-15 13:48:21 +0900

    meta tag change meta name => meta property

--- components/Head.js
+++ components/Head.js
@@ -6,24 +6,26 @@ const HeadCommon = ({ meta }) => {
     <Head>
       <title>{meta ? meta.displayName : "재일이의 넥스트js"}</title>
       <meta
-        name="og:title"
+        property="og:title"
         content={meta ? meta.displayName : "재일이의 넥스트js"}
       />
       <meta
-        name="og:image"
+        property="og:image"
         content={
           meta
             ? meta.imageUrl
             : "https://blog.kakaocdn.net/dn/bqCsrL/btrr5VxRKxV/adusUrS4nWZt0SNfY48yU0/img.jpg"
         }
+        key="ogimage"
       />
       <meta
-        name="og:description"
+        property="og:description"
         content={meta ? meta.discountedPrice : "넥스트 연습 desc og"}
+        key="ogdesc"
       />
-      <meta name="keyword" content="넥스트 연습 keyword" />
+      <meta property="keyword" content="넥스트 연습 keyword" />
       <meta
-        name="description"
+        property="description"
         content={meta ? meta.price : "넥스트 연습 desc og"}
       />
       <meta name="author" content="jaeilnet" />
